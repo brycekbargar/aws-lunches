a1ca560f41c64693153c52c4995eea30e8eae2c2
bryce <brycekbargar@gmail.com>
2022-10-06 21:20:50 -0400

    Try connecting to ec2 instance using ssh

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -14,7 +14,24 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "tls_private_key" "web_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+output "web_private_key" {
+  value     = tls_private_key.web_key.private_key_pem
+  sensitive = true
+}
+
+resource "aws_key_pair" "web_key" {
+  key_name   = "web_key"
+  public_key = tls_private_key.web_key.public_key_openssh
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+
+  key_name = aws_key_pair.web_key.key_name
 }
--- infra/provider.tf
+++ infra/provider.tf
@@ -4,10 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
+  profile = "aws-lunch-tf"
   default_tags {
     tags = {
       terraform = "true"
