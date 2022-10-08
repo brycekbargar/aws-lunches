564bcc94286775078ce5f9de3a4437a6fd2a8a4b
bryce <brycekbargar@gmail.com>
2022-10-08 18:01:45 -0400

    Install wordpress using ansible

--- infra/ansible.tf
+++ infra/ansible.tf
@@ -9,6 +9,15 @@ resource "local_sensitive_file" "web_key" {
   content              = tls_private_key.web_key.private_key_pem
 }
 
+resource "random_password" "web" {
+  count   = 2
+  length  = 31
+  special = true
+  keepers = {
+    key_value = aws_instance.hello_world_web.arn
+  }
+}
+
 resource "local_file" "inventory" {
   filename             = "${path.root}/../playbooks/terraform.yaml"
   directory_permission = "0755"
@@ -20,5 +29,7 @@ terraform:
       ansible_host: ${aws_instance.hello_world_web.public_dns}
       ansible_user: ubuntu
       ansible_ssh_private_key_file: ${local.web_key_pem}
+      mysql_root_password: ${random_password.web[0].result}
+      wordpress_db_password: ${random_password.web[1].result}
 INVENTORY
 }
